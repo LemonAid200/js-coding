e7d68cb78b6b543124a8e4c7a0f4b6f6d276202d
Vsevolod <vsevolodehe@mail.ru>
2022-02-02 18:17:53 +0500

    added main index page to ru;e 'em all

--- color changer/app.js
+++ color changer/app.js
@@ -1,30 +1,51 @@
- // Sat Jan 29 2022 17:51 adding a button that randomly changes pages color and tells the colors id
  date = new Date()
  console.log(date)
 
-
-let bit_nums = ["1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
-const button = document.getElementById("btn")
-const color = document.querySelector(".color")
+ let disko_flag = true
 
 
- button.addEventListener("click", ()=>{
-    for (let i =0; i<100; i++)
+
+
+
+ let bit_nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+ const button = document.getElementById("btn")
+ const auto_button = document.getElementById("autochange")
+ const color = document.querySelector(".color")
+
+
+ button.addEventListener("click", () => {
     new_color = getRandomColor()
     color.textContent = new_color
-    document.body.style.backgroundColor = new_color    
+    document.body.style.backgroundColor = new_color
  })
 
- function getRandomColor(){
+
+ function getRandomColor() {
     let color = "#"
-    for (let i = 0; i < 6; i++){
-        color += bit_nums[Math.floor(Math.random()*bit_nums.length)]
+    for (let i = 0; i < 6; i++) {
+       color += bit_nums[Math.floor(Math.random() * bit_nums.length)]
     }
     return color
  }
 
- document.addEventListener('keydown', function(event) {
-    if (event.code == 'KeyZ' && (event.ctrlKey || event.metaKey)) {
-      alert('Отменить!')
+
+ function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+ async function demo() {
+    console.log('Taking a break...');
+    await sleep(1000);
+    console.log('Two seconds later, showing sleep in a loop...');
+ }
+
+ auto_button.addEventListener("click", () => {
+    console.log("до цикла")
+    for (let a; a<10; a++) {
+       console.log("В цикле")
+       new_color = getRandomColor()
+       color.textContent = new_color
+       document.body.style.backgroundColor = new_color
+       demo()
     }
-  });
+ })
